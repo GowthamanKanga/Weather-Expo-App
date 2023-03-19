c2ff0278c47d9908d3b8c11fc1bac448e97134ed
GowthamanKanga <83148850+GowthamanKanga@users.noreply.github.com>
2023-03-19 17:13:44 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,56 @@
+import { StatusBar } from 'expo-status-bar';
+import React, {useEffect, useState} from 'react';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+
+import DateTime from './components/DateTime';
+import WeatherScroll from './components/WeatherScroll';
+
+const API_KEY = '49cc8c821cd2aff9af04c9f98c36eb74';
+
+const img = require('./assets/image.png')
+
+export default function App() {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((success) => {
+    
+        let {latitude, longtitude} = success.coords;
+        fetchDataFromApi(latitude, longtitude)
+
+    }, (err) => {
+      if(err) {
+        fetchDataFromApi("43.6532", "-79.3832")
+      }
+    })
+  }, [])
+
+  const fetchDataFromApi = (latitude, longtitude) => {
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longtitude}&exclude=hourly,minutely&units=metric&appid=${API_KEY}`)
+          .then(res => res.json())
+          .then(data => {
+            console.log(data)
+            setData(data)
+          })
+  }
+
+  return (
+    <View style={styles.container}>
+      <ImageBackground source={img} style={styles.image}>
+        <DateTime current={data.current} timezone={data.timezone} lat={data.lat} lon={data.lon}/>
+        <WeatherScroll weatherData={data.daily} />
+      </ImageBackground>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    flex:1, 
+    resizeMode: "cover", 
+    justifyContent: "center"
+  }
+});
